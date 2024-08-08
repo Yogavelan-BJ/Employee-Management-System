b96c7fc595c31cc5dde8827e9937073f0cc07c39
Yogavelan-BJ <122815384+Yogavelan-BJ@users.noreply.github.com>
2024-08-08 21:37:54 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyDVe71DJ-96sLEfQSr6EzOqt7WKjDpIjYI",
-  authDomain: "employee-management-syst-6dc35.firebaseapp.com",
-  projectId: "employee-management-syst-6dc35",
-  storageBucket: "employee-management-syst-6dc35.appspot.com",
-  messagingSenderId: "194183385848",
-  appId: "1:194183385848:web:d22f517c0227cdda24e10d",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
